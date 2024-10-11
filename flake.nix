af220e6b8e0b8b66bf50da1f70b465bf8ca80296
example <example@example.com>
2024-10-11 22:18:53 +0800

    PUSH

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+  inputs.naersk.url = "github:nix-community/naersk";
+
+  outputs = {
+    self, nixpkgs, flake-utils, rust-overlay, naersk
+  }: flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ (import rust-overlay) ];
+    };
+    rust = pkgs.rust-bin.nightly.latest.default;
+    naersk' = pkgs.callPackage naersk {
+      cargo = rust;
+      rustc = rust;
+    };
+  in {
+    packages.default = naersk'.buildPackage { src = ./.; };
+    devShells.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [ rust ];
+    };
+  });
+}
